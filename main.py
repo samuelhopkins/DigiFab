a0965d3b808591b023c9d71603b918d56003acb6
yunchenliu <yunchen@uchicago.edu>
2016-04-25 21:24:06 -0500

    Fixed Tabbing

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class Facet:
   def maxY(self):
     max = 0
     for v in self.vs:
-      if max < v.y
+      if max < v.y:
         max = v.y
     return max
   
@@ -40,21 +40,21 @@ class Facet:
         max = v.z
     return max
    
-   def minX(self):
+  def minX(self):
     min = 0
     for v in self.vs:
       if min < v.X:
         min = v.X
     return min
     
-    def minY(self):
+  def minY(self):
     min = 0
     for v in self.vs:
       if min < v.y:
         min = v.y
     return min
     
-   def minZ(self):
+  def minZ(self):
     min = 0
     for v in self.vs:
       if min > v.z:
@@ -73,10 +73,10 @@ def shell(shell_no, perim, output, thickness, count):
     for p in range(0,len(perim.pts)-2):
     	current = perim.pts[p]
     	dest = perim.pts[p+1]
-    	extrudate = extrudate + e.(dest, current)
+    	extrudate = extrudate + e(dest, current)
     	line = "G1 X%.3f Y%.3f E%.5f F%.3f" % (dest.x, dest.y, extrudate, 1800)
     	output.write(line)
-    extrudate = extrudate + e.(perim.pts[len(perim.pts)-1], perim.pts[0])
+    extrudate = extrudate + e(perim.pts[len(perim.pts)-1], perim.pts[0])
     line = "G1 X%.3f Y%.3f E%.5f" % (perim.pts[0].x, perim.pts[0].y, extrudate)
     output.write(line)
     line = "G1 %X.3f Y%.3f" % (perim.pts[0].x + s*thickness, perim.pts[0].y + s*thickness)
@@ -85,24 +85,24 @@ def shell(shell_no, perim, output, thickness, count):
     
 def parseFile(file):
   ret = []
-	f = open(self.file, "r")
-	line = f.readline()
-	while "endsolid" not in line:
-		facet = Facet()
-		line = f.readline()
-	  while "endfacet" not in line:
-			if "facet normal" in line:
-			  tokens = line.split(" ")
-				facet.normal = Vertex(tokens[2], tokens[3], tokens[4])
-			elif "vertex" in line:
-				tokens = line.split(" ")
-				facet.vs.append(tokens[1])
-				facet.vs.append(tokens[2])
-				facet.vs.append(tokens[3])
-			else:
-				pass
-		ret.append(facet)
-	f.close()
+  f = open(self.file, "r")
+  line = f.readline()
+  while "endsolid" not in line:
+    facet = Facet()
+    line = f.readline()
+    while "endfacet" not in line:
+      if "facet normal" in line:
+        tokens = line.split(" ")
+        facet.normal = Vertex(tokens[2], tokens[3], tokens[4])
+      elif "vertex" in line:
+        tokens = line.split(" ")
+        facet.vs.append(tokens[1])
+        facet.vs.append(tokens[2])
+        facet.vs.append(tokens[3])
+      else:
+        pass
+    ret.append(facet)
+  f.close()
   return ret
 
 def setF(extrude):
@@ -110,17 +110,17 @@ def setF(extrude):
    else: return 2400
 
 def infiller(infill_lines, extrude, output):
- for i in range(1,len(infill_lines)-1):
-   dest = infill_lines[i+1]
-   if extrude == 0:
-   	line = "G0 X%.2E Y%.2E F%.2E" % (dest.x, dest.y, 2400)
-   	output.write(line)
-   	extrude = 1
-   else:
-   	e = Infill.distance(i, dest)
-   	line = "G0 X%.2E Y%.2E E%.2E F%.2E" % (dest.x, dest.y, e, 1800)
-	output.write(line)
-	extrude = 0
+  for i in range(1,len(infill_lines)-1):
+    dest = infill_lines[i+1]
+    if extrude == 0:
+   	  line = "G0 X%.2E Y%.2E F%.2E" % (dest.x, dest.y, 2400)
+   	  output.write(line)
+   	  extrude = 1
+    else:
+   	  e = Infill.distance(i, dest)
+   	  line = "G0 X%.2E Y%.2E E%.2E F%.2E" % (dest.x, dest.y, e, 1800)
+    output.write(line)
+    extrude = 0
 
 
 def main(self):
@@ -139,7 +139,7 @@ def main(self):
   #gcode generator for supports
  
   #find height of given solid
-  height = 0:
+  height = 0
   for f in facets:
     max = f.maxZ()
     if height < max:
@@ -168,18 +168,19 @@ def main(self):
   #     do shell infill
   #     do infill code
 	#writing end-gcode
-	output.write(";End GCode"
-		     "M104 S0                     ;extruder heater off"
-		     "M140 S0                     ;heated bed heater off (if you have it)"
-		     "G91                                    ;relative positioning"
-		     "G1 E-1 F300                            ;retract the filament a bit before lifting the nozzle, to release some of the pressure"
-		     "G1 Z+0.5 E-5 X-20 Y-20 F{travel_speed} ;move Z up a bit and retract filament even more"
-		     "G28 X0 Y0                              ;move X/Y to min endstops, so the head is out of the way"
-		     "M84                         ;steppers off"
-		     "G90                         ;absolute positioning"
-		     ";{profile_string}")
+output.write(";End GCode"
+               "M104 S0                     ;extruder heater off"
+               "M140 S0                     ;heated bed heater off (if you have it)"
+      		     "G91                                    ;relative positioning"
+      		     "G1 E-1 F300                            ;retract the filament a bit before lifting the nozzle, to release some of the pressure"
+      		     "G1 Z+0.5 E-5 X-20 Y-20 F{travel_speed} ;move Z up a bit and retract filament even more"
+      		     "G28 X0 Y0                              ;move X/Y to min endstops, so the head is out of the way"
+      		     "M84                         ;steppers off"
+      		     "G90                         ;absolute positioning"
+      		     ";{profile_string}")
 	
-		     
+
+main.py
 
 
 
