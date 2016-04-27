fbe5507c451edfebe23f608b405a46141ac4c8bc
Skwaug <agonzalez8@uchicago.edu>
2016-04-26 21:38:33 -0500

    Update intersection.py

--- intersection.py
+++ intersection.py
@@ -16,21 +16,24 @@ def planeIntersect(p1,p2,zval):
 
 	#plane definitely doesn't cross line seg
 	if not (((p1.z <= zval) and (p2.z >= zval))
-		 or ((p1.z >= zval) and (p2.z <= zval))):
+		or ((p1.z >= zval) and (p2.z <= zval))):
+		print "no intersection"
 		return Vertex(0,0,-2)
 
 	x = (p1.x, p2.x-p1.x)
 	y = (p1.y, p2.y-p1.y)
 	z = (p1.z, p2.z-p1.z)
-	t = (zval-z[0])/z[1]
 	if z[1] == 0:
 		if z[0] == zval:
 			#line is on plane
+			print "line is on the plane"
 			return Vertex(0,0,-1)
 		else:
 			#line parallel to plane
+			print "line parallel to plane"
 			return Vertex(0,0,-2)
 	else:
+		t = (zval-z[0])/z[1]
 		return Vertex(x[0]+x[1]*t, y[0]+y[1]*t, z[0]+z[1]*t)
 
 
@@ -42,30 +45,40 @@ def facetIntersect(v1,v2,v3,zval):
 	 p2 = planeIntersect(v2,v3,zval)
 	 p3 = planeIntersect(v3,v1,zval)
 	 ret = []
-
 	 #cases where one edge of the triangle lies on the plane
 	 #    or the triangle is on the plane
 	 if p1.z == -1:
 	 	ret.append(Line(v1,v2))
+		print "Line v1,v2: " 
+		Line(v1,v2).show()
 	 if p2.z == -1:
 	 	ret.append(Line(v2,v3))
+		print "Line v2,v3:"
+		Line(v2,v3).show()
 	 if p3.z == -1:
 	 	ret.append(Line(v3,v1))
-
-	 if len(ret) > 0:
+		print "Line v3, v1:"
+		Line(v3,v1).show()
+	 print len(ret)
+	 if len(ret) == 0:
+		print "ret empty"
 	 	return ret
 
 	 #case where the triangle doesn't intersect the plane at all
 	 if p1.y == -2 and p2.y == -2 and p3.y == -2:
+		print "triangle doesn't intersect plane"
 	 	return ret
 
 	 #cases where the plane intersects the middle of the triangle 
 	 #   and one edge does not intersect the plane
 	 if p1.z == -2:
 	 	ret.append(Line(v1,v2))
+		print "Middle"
 	 elif p2.z == -2:
+		print "MIddle"
 	 	ret.append(Line(v2,v3))
 	 elif p3.z == -2:
+		print "Middle"
 	 	ret.append(Line(v3,v1))
 	 else:
 	 	tba = list(set([p1,p2,p3]))
